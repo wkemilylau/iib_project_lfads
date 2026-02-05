3788a47d40779a9e2db8c1744a5deec791fa2d4e
emilylauwk <emilyl1021@gmail.com>
2026-02-05 16:04:59 +0000

    inferred input analysis

---
+++ scripts/run_single_pag.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+from datetime import datetime
+from pathlib import Path
+
+from lfads_torch.run_model import run_model
+
+# ---------- OPTIONS -----------
+PROJECT_STR = "pag"
+DATASET_STR = "PAG"
+RUN_TAG = datetime.now().strftime("%y%m%d") + "_1"
+RUN_DIR = Path("runs") / PROJECT_STR / DATASET_STR / RUN_TAG
+OVERWRITE = True
+# ------------------------------
+
+# Overwrite the directory if necessary
+if RUN_DIR.exists() and OVERWRITE:
+    shutil.rmtree(RUN_DIR)
+RUN_DIR.mkdir(parents=True)
+# Copy this script into the run directory
+shutil.copyfile(__file__, RUN_DIR / Path(__file__).name)
+# Switch to the `RUN_DIR` and train the model
+os.chdir(RUN_DIR)
+run_model(
+    overrides={
+        "datamodule": DATASET_STR,
+        "model": DATASET_STR,
+    },
+    config_path="../configs/single_pag.yaml",
+)
