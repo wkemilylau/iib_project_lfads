9c0c55b798be16bb698b9428ceb14a525ea3e998
emilylauwk <emilyl1021@gmail.com>
2026-03-22 17:28:53 +0000

    v1 model and prelim eval

--- scripts/run_single_v1.py
+++ scripts/run_single_v1.py
@@ -8,7 +8,7 @@ from lfads_torch.run_model import run_model
 # ---------- OPTIONS -----------
 PROJECT_STR = "v1"
 DATASET_STR = "v1"
-RUN_TAG = datetime.now().strftime("%y%m%d") + "_1"
+RUN_TAG = datetime.now().strftime("%y%m%d") + "_5"
 RUN_DIR = Path("runs") / PROJECT_STR / DATASET_STR / RUN_TAG
 OVERWRITE = True
 # ------------------------------
